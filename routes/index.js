73db11f902d1575c44e2244d8e7feef11de828f1
Dhananjay shinde <dhanajayshinde2212@gmail.com>
2023-07-01 23:44:58 +0530

    app.js updated

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send("router get method")
+  
 });
 
 module.exports = router;
