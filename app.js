9dbb64acfdada82f0798d55f7413ff03c8e9ba49
Dhananjay shinde <dhanajayshinde2212@gmail.com>
2023-07-08 01:50:10 +0530

    helmet library implemented for avoiding clickJacking and cross site scripting

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 const app = express();
 
@@ -7,6 +8,13 @@ const indexRouter = require('./routes/index');
 const userRouter  = require('./routes/users');
 const requestLogger = require('./utilities/requestLogger');
 const errorLogger = require('./utilities/errorLogger')
+
+app.use(helmet());
+app.use((req, res, next) => {
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    next();
+  });
+app.use(helmet.frameguard());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
